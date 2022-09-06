2320fdb0105bc87837c2e3aded286dd37a482afc
Naganishe <mr.nordakov@mail.ru>
2022-09-06 23:37:11 +0300

    add leson version

--- Quest#2.py
+++ Quest#2.py
@@ -1,11 +1,8 @@
 # # 1. Напишите программу для. проверки истинности утверждения
 # #   ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-X = True
-Y = True
-Z = True
 
-if not(X or Y or Z) == (not X and not Y and not Z):
-    print(True)
-else:
-    print(False)
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x or y or z) == (not(x) and not(y) and not(z)))
